fe2d50c9c5c4f2a8d6158b1ce614ec82387d4dca
owerl1n <owerl1n.2@gmail.com>
2026-06-24 17:32:50 +0200

    modified:   Source/game/spike.lua

--- Source/game/spike.lua
+++ Source/game/spike.lua
@@ -8,18 +8,18 @@ local tileset = gfx.imagetable.new("level/tileset-table-16-16")
 assert(tileset, "Spike: не удалось загрузить tileset")
 
 local CONFIGS = {
-    small  = { frame = 443, collider = { x=2, y=10, w=12, h=6  } },
-    medium = { frame = 529, collider = { x=1, y=6,  w=14, h=10 } },
-    large  = { frame = 2211, collider = { x=0, y=2,  w=16, h=14 } },
+    Small  = { frame = 444, collider = { x=2, y=10, w=12, h=6  } },
+    Medium = { frame = 530, collider = { x=1, y=6,  w=14, h=10 } },
+    Large  = { frame = 2212, collider = { x=0, y=2,  w=16, h=14 } },
 }
 
 function Spike:init(x, y, entity)
     Spike.super.init(self)
-    print("aye")
-    local size   = entity.fields.size or "medium"
-    local config = CONFIGS[size] or CONFIGS.medium
 
-    self:setImage(imagetable[config.frame])
+    local size = entity.fields.size
+    local config = CONFIGS[size]
+
+    self:setImage(tileset[config.frame])
     self:setCenter(0, 0)
     self:moveTo(x, y)
     self:setZIndex(Z_INDEXES.Hazard)
